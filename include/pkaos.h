b6400ad48447c575761ad93b951a06ae255d7c8a
TunNgc <tuanngoc0906@gmail.com>
2026-04-04 00:47:15 +0700

    format

--- include/pkaos.h
+++ include/pkaos.h
@@ -20,10 +20,10 @@ void backspace();
 typedef struct {
     uint16_t limit_low;
     uint16_t base_low;
-    uint8_t base_middle;
-    uint8_t access;
-    uint8_t granularity;
-    uint8_t base_high;
+    uint8_t  base_middle;
+    uint8_t  access;
+    uint8_t  granularity;
+    uint8_t  base_high;
 } __attribute__((packed)) gdt_entry_t;
 
 typedef struct {
@@ -35,8 +35,8 @@ typedef struct {
 typedef struct {
     uint16_t base_lo;
     uint16_t sel;
-    uint8_t always0;
-    uint8_t flags;
+    uint8_t  always0;
+    uint8_t  flags;
     uint16_t base_hi;
 } __attribute__((packed)) idt_entry_t;
 
