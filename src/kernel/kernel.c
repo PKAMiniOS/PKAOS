e9b7f1aace72e5b4d0a8ebcbe9b5d1984bade605
Gnav <24100262@st.phenikaa-uni.edu.vn>
2026-04-04 19:56:34 +0700

    Restore kernel.c

---
+++ src/kernel/kernel.c
@@ -0,0 +1,19 @@
+#include "pkaos.h"
+
+void kernel_main() {
+    clear_screen();
+    print("=== Welcome to PKAOS ===\n\n");
+    // Treo máy ở đây vì chưa có ngắt bàn phím
+    
+    gdt_init();
+    idt_init(); 
+    isr_init();
+
+    init_keyboard();
+    asm volatile("sti");
+
+    while(1){
+        asm volatile("hlt");
+    }
+}
+
